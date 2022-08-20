f890c1e525248d490af4a851bce1e49b2c5b6642
Bruno Goncalves <bigbruno@gmail.com>
2022-08-20 20:48:45 -0300

    pamac installer fixed in bigbashview

--- bigcontrolcenter/usr/share/bigbashview/bcc/apps/waydroid/install.sh
+++ bigcontrolcenter/usr/share/bigbashview/bcc/apps/waydroid/install.sh
@@ -1,7 +1,55 @@
 #!/bin/bash
 
-pamac-installer --build waydroid-image
-pamac-installer waydroid waydroid-meta-x11
+#Install
+pamac-installer --build $1 &
+
+
+# Fix installation in BBV window
+PID="$!"
+
+if [ "$PID" = "" ]; then
+    exit
+fi
+
+CONTADOR=0
+while [  $CONTADOR -lt 100 ]; do
+    if [ "$(wmctrl -p -l | grep -m1 " $PID " | cut -f1 -d" ")" != "" ]; then
+        xsetprop -id=$(wmctrl -p -l | grep -m1 " $PID " | cut -f1 -d" ") --atom WM_TRANSIENT_FOR --value $(wmctrl -p -l -x | grep Waydroid$ | cut -f1 -d" ") -f 32x
+        wmctrl -i -r $(wmctrl -p -l | grep -m1 " $PID " | cut -f1 -d" ") -b add,skip_pager,skip_taskbar
+        wmctrl -i -r $(wmctrl -p -l | grep -m1 " $PID " | cut -f1 -d" ") -b toggle,modal
+        break
+    fi
+
+    sleep 0.1
+    let CONTADOR=CONTADOR+1; 
+done
+
+wait
+
+pamac-installer waydroid waydroid-meta-x11 &
+
+PID="$!"
+
+if [ "$PID" = "" ]; then
+    exit
+fi
+
+CONTADOR=0
+while [  $CONTADOR -lt 100 ]; do
+    if [ "$(wmctrl -p -l | grep -m1 " $PID " | cut -f1 -d" ")" != "" ]; then
+        xsetprop -id=$(wmctrl -p -l | grep -m1 " $PID " | cut -f1 -d" ") --atom WM_TRANSIENT_FOR --value $(wmctrl -p -l -x | grep Waydroid$ | cut -f1 -d" ") -f 32x
+        wmctrl -i -r $(wmctrl -p -l | grep -m1 " $PID " | cut -f1 -d" ") -b add,skip_pager,skip_taskbar
+        wmctrl -i -r $(wmctrl -p -l | grep -m1 " $PID " | cut -f1 -d" ") -b toggle,modal
+        break
+    fi
+
+    sleep 0.1
+    let CONTADOR=CONTADOR+1; 
+done
+
+wait
+
+
 
 if [ "$(lsmod | grep nvidia_drm)" != "" ]; then
     waydroid-big-3d 0
