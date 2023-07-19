abb50a73021b671f0e17832052e596f2f2d27231
vcatafesta <vcatafesta@gmail.com>
2023-07-19 13:42:43 -0400

    qua 19 jul 2023 13:42:43 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- bigcontrolcenter/usr/share/bigbashview/bcc/apps/bigcontrolcenter/kscreen_clean.sh
+++ bigcontrolcenter/usr/share/bigbashview/bcc/apps/bigcontrolcenter/kscreen_clean.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #Translation
 export TEXTDOMAINDIR="/usr/share/locale"
 export TEXTDOMAIN=bigcontrolcenter
 
 rm -Rf ~/.local/share/kscreen
-
 kdialog --msgbox $"As configurações da tela foram resetadas." --title $"Configurações da tela"
