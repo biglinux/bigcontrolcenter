4bb74389e7c52d1377dcaa8362def3168d2a153f
Bruno Gonçalves <bigbruno@gmail.com>
2024-06-23 04:01:16 -0300

    Add a simple cache method to generate .json content while bigbashview is starting

--- bigcontrolcenter/usr/share/bigbashview/apps/bigcontrolcenter/get_json.sh
+++ bigcontrolcenter/usr/share/bigbashview/apps/bigcontrolcenter/get_json.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+# If the first argument is "cache", then cache the json file
+if [[ $1 = "cache" ]]; then
+    python getappinfo.py $(./loop-search.sh | sed -E 's|.*/(.*).desktop|\1|g') android-usb ios-usb network-connect kcm_users timeshift-gtk big-themes-gui > ~/.cache/bigcontrolcenter.json
+    rm ~/.cache/pre-cache-bigcontrolcenter.json
+    exit
+fi
+
+# Wait for pre-cache file is deleted, if it is not deleted in 5 seconds, display without cache
+for i in {1..50}; do
+    if [ ! -f "$HOME/.cache/pre-cache-bigcontrolcenter.json" ]; then
+        echo "$(< ~/.cache/bigcontrolcenter.json)"
+        exit 0
+    fi
+    sleep 0.1
+done
+
 python getappinfo.py $(./loop-search.sh | sed -E 's|.*/(.*).desktop|\1|g') android-usb ios-usb network-connect kcm_users timeshift-gtk big-themes-gui
